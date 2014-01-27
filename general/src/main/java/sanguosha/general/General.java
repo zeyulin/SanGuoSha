1baa1ce386394bcbfcc7bdbd1888943d136123a7
zeyulin <linzeyu.fr@gmail.com>
2014-01-27 13:50:38 +0100

    modify general and player

--- general/src/main/java/sanguosha/general/General.java
+++ general/src/main/java/sanguosha/general/General.java
@@ -1,13 +1,13 @@
 package sanguosha.general;
 
-enum Power{
+enum State{
     WEI,SHU,WU,QUN
 }
 
 public class General {
     public String name;
     public short physical;
-    public Power power;
+    public State power;
 
 
     //skills
--- player/src/main/java/sanguosha/player/Player.java
+++ player/src/main/java/sanguosha/player/Player.java
@@ -1,7 +1,10 @@
 package sanguosha.player;
 
 import sanguosha.card.Card;
+import sanguosha.equipment.Equipments;
 import sanguosha.general.General;
+
+import java.util.List;
 
 enum Faction{
     ZHU_GONG,ZHONG_CHEN,FAN_ZEI,NEI_JIAN
@@ -9,8 +12,19 @@ enum Faction{
 
 public class Player {
     public short position;
-    public short faction;
+    public short physical;
+    public Faction faction;
     public General general;
-    public Card card;
+
+
+    //status buffers
+    //le,bing,shandian
+
+
+    //equipment
+    public Equipments equipments;
+    public List<Card> cards;                   //......................................................TODO
+
+
 
 }
