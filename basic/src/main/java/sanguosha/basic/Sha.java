3a0855305a0f3a2904e11cdbbcbdd890db8abc04
zeyulin <linzeyu.fr@gmail.com>
2014-01-27 13:48:54 +0100

    add module basic

---
+++ basic/src/main/java/sanguosha/basic/Sha.java
@@ -0,0 +1,14 @@
+package sanguosha.basic;
+
+import sanguosha.player.Player;
+
+public class Sha {
+    //player
+    public Player fromPlayer;
+    public Player toPlayer;
+    public Player sourcePlayer;
+    public Player targetPlayer;
+
+    public short demage;
+
+}
---
+++ basic/src/main/java/sanguosha/basic/Shan.java
@@ -0,0 +1,11 @@
+package sanguosha.basic;
+
+import sanguosha.player.Player;
+
+public class Shan {
+    //player
+    public Player fromPlayer;
+    public Player toPlayer;
+    public Player sourcePlayer;
+    public Player targetPlayer;
+}
